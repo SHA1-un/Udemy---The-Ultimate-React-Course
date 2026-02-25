62433248d92a3899d3aaf2af7d25b8e884500d04
Shaun <shaun.oosthuizen.wp@gmail.com>
2026-02-25 20:37:39 +0200

    Added section 8 starting project

--- 2026/Section 5 React Essentials - Practice Project/src/components/InputField.jsx
+++ 2026/Section 5 React Essentials - Practice Project/src/components/InputField.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 export default function InputField({name, title, onChange}) {
-    const [userInput, setUserInput] = useState(0);
+    // const [userInput, setUserInput] = useState(0);
 
     function handleInput(event) {
         const input = event.target.value;
-        setUserInput(input);
+        // setUserInput(input);
         onChange(name, input);
     }
     
     return <span>
         <label>{title}</label>
-        <input type="number" onChange={handleInput} value={userInput} min={0} />
+        <input type="number" onChange={handleInput} value={name} min={0} />
     </span>
 }
