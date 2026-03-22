da263af515a8508d57196531795e27c10a0d5ad4
Shaun <shaun.oosthuizen.wp@gmail.com>
2026-03-22 11:20:20 +0200

    Progress commit

--- 2026/Section 8 Refs and Portals/src/components/Player.jsx
+++ 2026/Section 8 Refs and Portals/src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("Unknown entity")
+  const editPlayerName = useRef(playerName);
+
+  function handleNameChange(event) {
+    editPlayerName.current = event.target.value;
+  }
+
+  function saveName() {
+    if (!editPlayerName || !editPlayerName.current) return;
+    
+    setPlayerName(editPlayerName.current);
+    editPlayerName.current = "";
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleNameChange} value={editPlayerName.current}/>
+        <button onClick={saveName}>Set Name</button>
       </p>
     </section>
   );
