c2293aa7396f85022b869dd98653826819da0757
Shaun <shaun.oosthuizen.wp@gmail.com>
2026-04-02 18:35:33 +0200

    Added state handling and basic UI and for project selection

--- 2026/Section 9 Project Management App/src/App.jsx
+++ 2026/Section 9 Project Management App/src/App.jsx
@@ -5,11 +5,43 @@ import { projects as sampleProjects } from './data/sampleProjects';
 
 function App() {
   const [projects, setProjects] = useState(sampleProjects);
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  function handleSave(_project) {
+    setProjects(prevProjects => {
+      // Create a deep copy of the projects
+      const updatedProjects = prevProjects.map(prevProject => {
+        return {
+          ...prevProject,
+          tasks: [...prevProject.tasks]
+        }
+      });
+      
+      // Check if project exists 
+      const existingProject = projects.find(current_project => current_project.id === _project.id);
+      if (existingProject) { 
+        // Update existing entry
+      } else {
+        updatedProjects.push(_project);
+      }
+
+      return updatedProjects;
+    })
+  }
+
+  function handleProjectSelect(project) {
+    setSelectedProject(prevProject => {
+      if (prevProject && project.id === prevProject.id) return null;
+
+      return project;
+    })
+  }
+
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar projects={projects}></Sidebar>
-      <Project></Project>
+      <Sidebar projects={projects} selectedProject={selectedProject} handleProjectSelect={handleProjectSelect}></Sidebar>
+      <Project onSave={handleSave} selectedProject={selectedProject}></Project>
 
     </main>
   );
--- 2026/Section 9 Project Management App/src/components/Sidebar.jsx
+++ 2026/Section 9 Project Management App/src/components/Sidebar.jsx
@@ -4,15 +4,18 @@
  * 2. Adding a project will create a new project on the right side that he user can edit
  */
 
-export default function Sidebar({ projects }) {
+export default function Sidebar({ projects, selectedProject, handleProjectSelect }) {
     return <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
         <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
         <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">+ Add Project</button>
 
         <ul className="mt-8">
             {projects.map((project) => {
-                return <li key={project.id}>
-                    <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
+                const isSelected = selectedProject?.id === project.id;
+                let buttonClass = "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800"
+                if(isSelected) buttonClass += " text-stone-200 bg-stone-800";
+                return <li key={project.id} >
+                    <button onClick={() => handleProjectSelect(project)} className={buttonClass}>
                         {project.title}
                     </button>
                 </li>
