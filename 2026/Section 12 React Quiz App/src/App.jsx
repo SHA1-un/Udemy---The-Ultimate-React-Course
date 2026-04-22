7061a3483794e5078e92e2071531d9e1e5da7a41
Shaun <shaun.oosthuizen.wp@gmail.com>
2026-04-22 20:17:12 +0200

    Show questions in quiz component

--- 2026/Section 12 React Quiz App/src/App.jsx
+++ 2026/Section 12 React Quiz App/src/App.jsx
@@ -2,10 +2,12 @@ import Header from "./components/Header";
 import Quiz from "./components/Quiz";
 function App() {
     return (
-        <div>
+        <>
             <Header title="React Quiz"></Header>
-            <Quiz />
-        </div>
+            <main>
+                <Quiz />
+            </main>
+        </>
     )
 }
 
--- 2026/Section 12 React Quiz App/src/components/Quiz.jsx
+++ 2026/Section 12 React Quiz App/src/components/Quiz.jsx
@@ -3,33 +3,38 @@ import questions from "../data/questions";
 import ProgressBar from "./ProgressBar"
 
 export default function Quiz() {
+    const [answers, setAnswers] = useState([]);
+    const activeQuestionIndex = answers.length < questions.length ? answers.length : questions.length - 1;
+    const currentQuestion = questions[activeQuestionIndex];
 
-    // Idea: we would create a new context file to manage the current question index and answered questions
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const currentQuestion = questions[currentQuestionIndex];
+    function handleSelectAnswer(selectedAnswer) {
+        if (activeQuestionIndex === questions.length - 1) return;
 
-    function nextQuestion() {
-        setCurrentQuestionIndex(prevIndex => {
-            const nextIndex = prevIndex + 1;
-            if (nextIndex + 1 > questions.length) return prevIndex;
-
-            return nextIndex
+        setAnswers(prevAnswers => {
+            return [
+                ...prevAnswers,
+                selectedAnswer
+            ];
         });
     }
 
     return (
-        <div>
+        <>
             <ProgressBar />
-            <p>{currentQuestionIndex + 1}. {currentQuestion.text}</p>
-            <ul>
-                {currentQuestion.answers.map((answer) => {
-                    return <li>
-                        <button onClick={nextQuestion}>
-                            {answer}
-                        </button>
-                    </li>
-                })}
-            </ul>
-        </div>
+            <div id="quiz">
+                <div id="question">
+                    <h2>{activeQuestionIndex + 1}. {currentQuestion.text}</h2>
+                    <ul id="answers">
+                        {currentQuestion.answers.map((answer, index) => {
+                            return <li key={index} className="answer">
+                                <button onClick={() => handleSelectAnswer(answer)}>
+                                    {answer}
+                                </button>
+                            </li>
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </>
     )
 }
