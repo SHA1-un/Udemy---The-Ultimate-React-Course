ecc5928f452699b80287a6057e1d15d6ae87f7f1
Shaun <shaun.oosthuizen.wp@gmail.com>
2026-04-24 06:36:42 +0200

    WIP - added progress bar logic. Check why progress bar does not reset after going to next question

--- 2026/Section 12 React Quiz App/src/components/Quiz.jsx
+++ 2026/Section 12 React Quiz App/src/components/Quiz.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import questions from "../data/questions";
 import ProgressBar from "./ProgressBar";
 import ResultModal from "./ResultModal";
 
+
+const QUESTION_TIME = 15000;
 export default function Quiz() {
     const [answers, setAnswers] = useState([]);
 
@@ -10,23 +12,32 @@ export default function Quiz() {
     const currentQuestion = questions[activeQuestionIndex];
     const quizOver = activeQuestionIndex === questions.length;
     
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback((selectedAnswer) => {
         setAnswers(prevAnswers => {
             return [
                 ...prevAnswers,
                 selectedAnswer
             ];
         });
-    }
+    }, []);
     
     if (quizOver) return <ResultModal />
 
     const shuffeldAnswers = [...currentQuestion.answers];
     shuffeldAnswers.sort(() => Math.random() - 0.5);
 
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            console.log("times up!");
+            handleSelectAnswer(null); // user took too long to select answer
+        }, QUESTION_TIME);
+
+        return () => clearTimeout(timeout);
+    }, [handleSelectAnswer]);
+
     return (
         <div id="quiz">
-            <ProgressBar />
+            <ProgressBar maxTime={QUESTION_TIME}/>
             <div id="question">
                 <h2>{activeQuestionIndex + 1}. {currentQuestion.text}</h2>
                 <ul id="answers">
