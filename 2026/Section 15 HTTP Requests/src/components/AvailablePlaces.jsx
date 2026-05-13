e561bd512f781defce73676fe4519f55ddf6fad0
Shaun <shaun.oosthuizen.wp@gmail.com>
2026-05-13 21:11:11 +0200

    Updated implementation to  make use of async/await

--- 2026/Section 15 HTTP Requests/src/components/AvailablePlaces.jsx
+++ 2026/Section 15 HTTP Requests/src/components/AvailablePlaces.jsx
@@ -5,12 +5,24 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [places, setPlaces] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then(response => {
-        if (!response.ok) throw new Error("Network Error");
-        return response.json();
-      }).then(data => setPlaces(data.places))
-      .catch(error => console.log(`Fetch error ${error}`))
+    // Alternative way to use fetch with async/await
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      if (!response.ok) throw new Error("Network Error");
+
+      const data = await response.json();
+
+      setPlaces(data.places);
+    }
+
+    fetchPlaces();
+
+    // fetch('http://localhost:3000/places')
+    //   .then(response => {
+    //     if (!response.ok) throw new Error("Network Error");
+    //     return
+    //   }).then(data => setPlaces(data.places))
+    //   .catch(error => console.log(`Fetch error ${error}`))
   }, []);
 
   return (
