f429a7067caedd2a5886951d324fd8a9219d81b7
Shaun <shaun.oosthuizen.wp@gmail.com>
2026-06-07 19:55:13 +0200

    Added Local storing of in progress cart and adding items. BUG: local storage cleared on reload - prob due to useEffect that runs on cart state change

--- 2026/Section 19 Food Delivery App/src/components/food-list/FoodCard.jsx
+++ 2026/Section 19 Food Delivery App/src/components/food-list/FoodCard.jsx
@@ -1,4 +1,8 @@
+import { useContext } from "react";
+import { CartContext } from "../../store/cart-context";
+
 export default function FoodCard({ foodItem }) {
+    const { addItem } = useContext(CartContext);
     return <li className="meal-item">
         <article>
             <img src={`http://localhost:3000/${foodItem.image}`} />
@@ -7,7 +11,7 @@ export default function FoodCard({ foodItem }) {
                 <p className="meal-item-price">{foodItem.price}</p>
                 <p className="meal-item-description">{foodItem.description}</p>
                 <div className="meal-item-actions">
-                    <button className="button" >Add to Cart</button>
+                    <button className="button" onClick={() => addItem(foodItem)}>Add to Cart</button>
                 </div>
             </section>
         </article>
--- 2026/Section 19 Food Delivery App/src/store/cart-context.jsx
+++ 2026/Section 19 Food Delivery App/src/store/cart-context.jsx
@@ -2,24 +2,52 @@ import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext({
     items: [],
-    addItem: () => {},
-    removeItem: () => {},
-    submitOrder: () => {}
+    addItem: () => { },
+    removeItem: () => { },
+    submitOrder: () => { }
 });
 
 export default function CartContextProvider({ children }) {
     const [cart, setCart] = useState([]);
 
     function loadCart() {
-        const userCart = localStorage.getItem("user-cart");
+        const localCart = localStorage.getItem("user-cart");
+        const parsedLocalCart = localCart ? JSON.parse(localCart) : [];
 
-        setCart(userCart || []);
+        setCart(parsedLocalCart);
+    }
+
+    function addItem(newItem) {
+        setCart(prevCart => {
+            const updatedCart = [...prevCart];
+            const cartItemIndex = updatedCart.findIndex(item => item.id === newItem.id);
+
+            // Update exsisting item
+            if (cartItemIndex >= 0) {
+                updatedCart[cartItemIndex] = {
+                    ...updatedCart[cartItemIndex],
+                    count: updatedCart[cartItemIndex].count + 1
+                }
+            } else { // Add new item
+                updatedCart.push({
+                    ...newItem,
+                    count: 1
+                });
+            }
+
+            return updatedCart
+        })
     }
 
     useEffect(() => { loadCart() }, []);
 
+    useEffect(() => {
+        localStorage.setItem("user-cart", JSON.stringify(cart))
+    }, [cart]);
+
     const cartCtx = {
-        items: cart
+        items: cart,
+        addItem
     };
 
     return <CartContext value={cartCtx}>
