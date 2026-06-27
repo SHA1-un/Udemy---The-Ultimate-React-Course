c431dbf7741d89d85575c8836b00b18ad2494af5
Shaun <shaun.oosthuizen.wp@gmail.com>
2026-06-27 07:36:06 +0200

    Added basic routing with navbar and children routes

--- 2026/Section 22 - React Routing/src/App.js
+++ 2026/Section 22 - React Routing/src/App.js
@@ -1,12 +1,20 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  {path: "/", element: <HomePage/>}
+  {
+    path: "/", element: < RootLayout/>,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <Products /> }
+    ]
+  },
 ]);
 
 function App() {
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- 2026/Section 22 - React Routing/src/pages/HomePage.jsx
+++ 2026/Section 22 - React Routing/src/pages/HomePage.jsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-    return <h1>HOME PAGE</h1>
+    return <>
+        <h1>HOME PAGE</h1>
+        <Link to="/products">
+            <button>Products</button>
+        </Link>
+    </>
 }
