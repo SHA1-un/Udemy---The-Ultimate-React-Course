171851be8f82f13a1715f574b02167b1d2320adf
Shaun <shaun.oosthuizen.wp@gmail.com>
2026-07-30 06:25:28 +0200

    Duplicated advanced example project

---
+++ 2026/Section 22 - React Routing/Advanced Routing Example (updated for useFetcher)/frontend/src/page/EditEventPage.jsx
@@ -0,0 +1,9 @@
+import { useRouteLoaderData } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
+export default function EditEventPage() {
+    const loaderData = useRouteLoaderData("event-details");
+    const event = loaderData.event;
+
+    return <EventForm method="patch" event={event}/>
+}
